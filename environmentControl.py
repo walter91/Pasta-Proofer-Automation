c3d88394cd802cd67113a99a60871f284a82a40c
walter91 <waltermcoe@gmail.com>
2016-01-12 14:56:45 -0700

    Laid out python control suedo-code. Started state diagram

---
+++ environmentControl.py
@@ -0,0 +1,18 @@
+#import modules
+
+#define GPIO control pins
+
+#define states through enum for humidity and temperature
+
+#check JSON database for desired temp and humidity values and tolerances and current state
+#define upper and lower temp and humidity thresholds
+
+#gather temperature and humidity readings
+
+#state machine to determine state
+
+#update GPIO
+
+#update JSON database
+
+#return
